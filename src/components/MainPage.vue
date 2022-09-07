6a8202eba189dd29947612be6af37f04fdf45b42
pat2304107 <pat0919304107@gmail.com>
2022-09-07 15:22:47 +0800

    adding

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="container">
     <img src="../assets/C81C83E2.jpeg" class="photo" />
-    <div class="name"></div>
-    <div class="introduction"></div>
+    <div class="name">
+      <h1>盧俊諺</h1>
+      <h2>CHUN-YEN LU</h2>
+    </div>
+    <div class="introduction">
+      畢業於國立臺灣藝術大學圖文傳播藝術學系。<br />
+      向來喜歡接觸新奇的事物，且具有高行動力。<br />
+      自認學習能力佳，能快速上手全新事物，享受解決問題的過程。
+    </div>
     <div class="gdw"></div>
     <div class="mdw"></div>
+    <div class="mail"></div>
   </div>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/LuLu_Portfolio/',
   plugins: [
     vue({
       input: ['resources/sass/app.scss', 'resources/js/app.js'],
