eef89b4d32a33e89623343820eda538edcac0083
pat2304107 <pat0919304107@gmail.com>
2022-09-07 21:29:34 +0800

    adding dist

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './assets/styles/style.scss';
-import '@fontsource/m-plus-1p/300.css';
-
-const app = createApp(App);
-app.mount('#app');
