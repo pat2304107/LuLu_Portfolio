02c056e98d0b6b911d4241f057fc250a1c7b7a18
pat2304107 <pat0919304107@gmail.com>
2022-09-12 15:39:36 +0800

    9/12 main page animated

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,42 +1,83 @@
 <template>
   <div class="container">
-    <div class="info">
+    <div class="info come" v-show="infoIsShow">
       <img src="../assets/C81C83E2.jpeg" class="photo" />
       <div class="name">
         <div class="name_ch">盧　俊諺</div>
         <div class="name_en">CHUN-YEN LU</div>
       </div>
     </div>
-    <div class="intro_block" data-aos="fade-up">
-      <div class="intro_L"><img src="../assets/關於我L.png" alt="" /></div>
+    <div class="intro_block come" v-show="introIsShow">
+      <div class="intro_L"><img src="../assets/intro_L.png" alt="" /></div>
       <div class="introduction">
         <div>畢業於國立臺灣藝術大學圖文傳播藝術學系。</div>
         <div>向來喜歡接觸新奇的事物，且具有高行動力。</div>
         <div>自認學習能力佳，能快速上手全新事物，享受解決問題的過程。</div>
       </div>
-      <div class="intro_R"><img src="../assets/關於我R.png" alt="" /></div>
+      <div class="intro_R">
+        <img src="../assets/intro_R.png" alt="" />
+      </div>
     </div>
-    <div class="dw_block">
-      <div class="dw_L"><img src="../assets/作品集L.png" /></div>
+    <div class="dw_block come" v-show="dwIsShow">
+      <div class="dw_L"><img src="../assets/dw_L.png" /></div>
       <a href="" class="gdw"><img src="../assets/GDW.png" /></a>
       <div class="mdw"><img src="../assets/MDW.png" /></div>
-      <div class="dw_R"><img src="../assets/作品集R.png" /></div>
+      <div class="dw_R"><img src="../assets/dw_R.png" /></div>
     </div>
-    <div class="mail_block">
-      <div class="mail_L"><img src="../assets/聯絡我L.png" /></div>
+    <div class="mail_block come" v-show="mailIsShow">
+      <div class="mail_L"><img src="../assets/mail_L.png" /></div>
       <div class="mail"><img src="../assets/mail.png" alt="" /></div>
-      <div class="mail_R"><img src="../assets/聯絡我R.png" /></div>
+      <div class="mail_R"><img src="../assets/mail_R.png" /></div>
     </div>
   </div>
 </template>
 
 <script>
+import $ from 'jquery';
+
 export default {
-  props: {},
-  components: {},
-  data() {},
+  data() {
+    return {
+      timer: '',
+      infoTimer: '',
+      introTimer: '',
+      dwTimer: '',
+      mailTimer: '',
+      infoIsShow: false,
+      introIsShow: false,
+      dwIsShow: false,
+      mailIsShow: false,
+    };
+  },
   computed() {},
-  mounted() {},
-  methods() {},
+  methods: {
+    comtainerAnimate: function () {
+      $('.container').animate(
+        {
+          height: '788px',
+        },
+        'slow'
+      );
+    },
+    infoShowing: function () {
+      this.infoIsShow = !this.infoIsShow;
+    },
+    introShowing: function () {
+      this.introIsShow = !this.introIsShow;
+    },
+    dwShowing: function () {
+      this.dwIsShow = !this.dwIsShow;
+    },
+    mailShowing: function () {
+      this.mailIsShow = !this.mailIsShow;
+    },
+  },
+  mounted() {
+    this.timer = setTimeout(this.comtainerAnimate, 500);
+    this.infoTimer = setTimeout(this.infoShowing, 1000);
+    this.introTimer = setTimeout(this.introShowing, 1500);
+    this.dwTimer = setTimeout(this.dwShowing, 2000);
+    this.mailTimer = setTimeout(this.mailShowing, 2500);
+  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import Aos from 'aos';
 import './assets/styles/style.scss';
 import '@fontsource/m-plus-1p/300.css';
-
-Aos.init();
 
 const app = createApp(App);
 app.mount('#app');
